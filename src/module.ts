5fdd87de993fc5c6db9ac92fb8bad52c65387656
NickRisinger <boozy044@gmail.com>
2025-01-12 20:29:45 +0300

    style: fix code style for eslint

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,18 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit';
 
-// Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+	id?: string;
+}
 
 export default defineNuxtModule<ModuleOptions>({
-  meta: {
-    name: 'nuxt-yandex-metrika',
-    configKey: 'yandexMetrika',
-  },
-  // Default configuration options of the Nuxt module
-  defaults: {},
-  setup(_options, _nuxt) {
-    const resolver = createResolver(import.meta.url)
+	meta: {
+		name: 'nuxt-yandex-metrika',
+		configKey: 'yandexMetrika',
+	},
+	defaults: {},
+	setup(_options, _nuxt) {
+		const resolver = createResolver(import.meta.url);
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
-  },
-})
+		addPlugin(resolver.resolve('./runtime/plugin'));
+	},
+});
